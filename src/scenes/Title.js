ad1885499dcffc7c910620c4a7d890322f01fcd6
jaxonRuiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-03-17 17:45:08 -0700

    oops
    i forgot to put things in the right git repo

---
+++ src/scenes/Title.js
@@ -0,0 +1,43 @@
+class Title extends Phaser.Scene {
+    constructor() {
+        super("titleScene")
+    }
+
+    preload() {
+        // load assets
+        this.load.path = "./assets/"
+
+        // load JSON (ie dialog text)
+        this.load.json('dialog', 'json/dialog.json')
+
+        // load images
+        this.load.image('dialogbox', 'img/dialogbox.png')
+        this.load.image('homer', 'img/homer.png')
+        this.load.image('minerva', 'img/minerva.png')
+        this.load.image('jove', 'img/jove.png')
+        this.load.image('neptune', 'img/neptune.png')
+        this.load.image('punpun', 'img/punpun.png')
+
+        // load bitmap font
+        this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
+        this.load.bitmapFont('help_font', 'font/Unnamed.png', 'font/Unnamed.xml') 
+        // im trying to simultaneously (misspelled) do class enrollment and am incredibly stressed. names are gonna be bad
+
+    }
+
+    create() {
+        // add title text
+        this.add.bitmapText(centerX, centerY - 32, 'help_font', 'THE ODYSSEY', 32).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY, 'help_font', 'Press SPACE to start', 16).setOrigin(0.5)
+
+        // create input
+        cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // wait for player input
+        if(Phaser.Input.Keyboard.JustDown(cursors.space)) {
+            this.scene.start("talkingScene")
+        }
+    }
+}
